5dcd154d2b75407e2c2d9c278d691f3e5c9bd44e
HoaiNam0710 <namnguyen@busmap.vn>
2025-09-06 15:06:04 +0700

    feat: compare text

--- src/components/feature/text-compare/DiffResult.tsx
+++ src/components/feature/text-compare/DiffResult.tsx
@@ -1,17 +1,82 @@
-interface DiffResultProps {
-  diffHtml: string;
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import I18n from "@/components/utils/I18n";
+import {
+    useDiffViewer,
+    useDiffViewerCompareByLine,
+} from "@/hooks/useDiffViewer";
+
+interface DiffViewerProps {
+  text1: string;
+  text2: string;
 }
 
-export const DiffResult = ({ diffHtml }: DiffResultProps) => {
-  if (!diffHtml) return null;
+export const InlineDiffViewer = ({ text1, text2 }: DiffViewerProps) => {
+  const { diffHtml } = useDiffViewer(text1, text2);
 
+  if (!text1 && !text2) {
+    return (
+      <div className="text-sm p-2 bg-gray-100 rounded-md overflow-auto h-full">
+        <pre className="text-gray-400">Empty</pre>
+      </div>
+    );
+  }
   return (
-    <div className="text-sm p-2 bg-gray-100 rounded-md overflow-auto h-full mt-4">
+    <div className="text-sm p-2 bg-gray-100 rounded-md overflow-auto h-full">
       <pre
         className="whitespace-pre-wrap font-sans"
         dangerouslySetInnerHTML={{ __html: diffHtml }}
       />
-      
     </div>
+  );
+};
+
+export const SideBySideDiffViewer: React.FC<DiffViewerProps> = ({
+  text1,
+  text2,
+}: DiffViewerProps) => {
+  const { oldHtml, newHtml } = useDiffViewerCompareByLine(text1, text2);
+  return (
+    <div className="flex gap-4">
+      <div className="w-1/2">
+        <div className="text-sm p-2 bg-gray-100 rounded-md overflow-auto h-64">
+          <pre
+            className="whitespace-pre-wrap font-sans"
+            dangerouslySetInnerHTML={{ __html: oldHtml }}
+          />
+        </div>
+      </div>
+
+      <div className="w-1/2">
+        <div className="text-sm p-2 bg-gray-100 rounded-md overflow-auto h-64">
+          <pre
+            className="whitespace-pre-wrap font-sans"
+            dangerouslySetInnerHTML={{ __html: newHtml }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const DiffViewerTabs = ({ text1, text2 }: DiffViewerProps) => {
+  return (
+    <>
+      <Tabs defaultValue="inline" className="mt-4">
+        <TabsList className="mb-2">
+          <TabsTrigger value="inline">
+            <I18n value="Inline" />
+          </TabsTrigger>
+          <TabsTrigger value="sideBySide">
+            <I18n value="Side by Side" />
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="inline">
+          <InlineDiffViewer text1={text1} text2={text2} />
+        </TabsContent>
+        <TabsContent value="sideBySide">
+          <SideBySideDiffViewer text1={text1} text2={text2} />
+        </TabsContent>
+      </Tabs>
+    </>
   );
 };
--- src/components/feature/text-compare/TextDiffComparator.tsx
+++ src/components/feature/text-compare/TextDiffComparator.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import I18n from "@/components/utils/I18n";
-import {
-    useDiffViewer,
-    useDiffViewerCompareByLine,
-} from "@/hooks/useDiffViewer";
 import { useState } from "react";
-import { DiffResult } from "./DiffResult";
+import { DiffViewerTabs } from "./DiffResult";
 import { TextInputForm } from "./TextInputForm";
 
 // Interface cho các tùy chọn so sánh
@@ -24,11 +19,6 @@ const TextDiffComparator = () => {
     text1: "",
     text2: "",
   });
-  const { diffHtml } = useDiffViewer(options.text1, options.text2);
-  const { oldHtml, newHtml } = useDiffViewerCompareByLine(
-    options.text1,
-    options.text2
-  );
 
   const handleSubmit = () => {
     setOptions({
@@ -46,32 +36,7 @@ const TextDiffComparator = () => {
         setText2={setText2}
         onSubmit={handleSubmit}
       />
-      <DiffResult diffHtml={diffHtml} />
-
-      <div className="mt-4 flex gap-4">
-        <div className="w-1/2">
-          <h3 className="font-semibold">
-            <I18n value="Văn bản cũ" />
-          </h3>
-          <div className="text-sm p-2 bg-gray-100 rounded-md overflow-auto h-64">
-            <pre
-              className="whitespace-pre-wrap font-sans"
-              dangerouslySetInnerHTML={{ __html: oldHtml }}
-            />
-          </div>
-        </div>
-        <div className="w-1/2">
-          <h3 className="font-semibold">
-            <I18n value="Văn bản mới" />
-          </h3>
-          <div className="text-sm p-2 bg-gray-100 rounded-md overflow-auto h-64">
-            <pre
-              className="whitespace-pre-wrap font-sans"
-              dangerouslySetInnerHTML={{ __html: newHtml }}
-            />
-          </div>
-        </div>
-      </div>
+      <DiffViewerTabs text1={options.text1} text2={options.text2} />
     </div>
   );
 };
